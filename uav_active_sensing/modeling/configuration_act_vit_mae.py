3ee144928b7c7e82c1ca0d648aa07458d92de88a
Tomás Couso <tcouso@uc.cl>
2025-02-05 17:04:57 +0900

    Feat: Farama interface on the way

--- uav_active_sensing/modeling/configuration_act_vit_mae.py
+++ uav_active_sensing/modeling/configuration_act_vit_mae.py
@@ -115,6 +115,9 @@ class ActViTMAEConfig(PretrainedConfig):
         decoder_intermediate_size=2048,
         # mask_ratio=0.75,
         norm_pix_loss=False,
+        v_max_x=10,
+        v_max_y=10,
+        v_max_z=2,
         **kwargs,
     ):
         super().__init__(**kwargs)
@@ -138,6 +141,9 @@ class ActViTMAEConfig(PretrainedConfig):
         self.decoder_intermediate_size = decoder_intermediate_size
         # self.mask_ratio = mask_ratio
         self.norm_pix_loss = norm_pix_loss
+        self.v_max_x = v_max_x
+        self.v_max_y = v_max_y
+        self.v_max_z = v_max_z
 
 
 __all__ = ["ActViTMAEConfig"]
