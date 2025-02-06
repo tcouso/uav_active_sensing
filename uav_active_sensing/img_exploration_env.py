c5995d969f0464e28a6dd1815fe9ebeb0a6e6af5
Tomás Couso <tcouso@uc.cl>
2025-02-06 17:00:58 +0900

    Feat: ImageExplorationEnv class with gym interface implemented

--- uav_active_sensing/img_exploration_env.py
+++ uav_active_sensing/img_exploration_env.py
@@ -1,11 +1,38 @@
 import random as rd
 from typing import Optional, Tuple, List
+import numpy as np
 
 import torch
 import torch.nn.functional as F
 
+import gymnasium as gym
+from gymnasium import spaces
 
-class ImageEnv:
+
+from uav_active_sensing.modeling.configuration_act_vit_mae import ActViTMAEConfig
+from uav_active_sensing.modeling.act_vit_mae import ActViTMAEForPreTraining
+
+
+def make_kernel_size_odd(n: int) -> int:
+    assert n > 0
+
+    return n if n % 2 == 1 else n - 1
+
+
+class RewardFunction:
+    def __init__(self, model):
+        self.model: ActViTMAEForPreTraining = model
+
+    def __call__(self, img: torch.Tensor, sampled_img: torch.Tensor) -> float:
+
+        outputs = self.model(img, sampled_img)
+        loss = outputs.loss
+        reward = 1 / (1 + loss)
+
+        return reward
+
+
+class ImageExplorationEnv(gym.Env):
     """
     A class to simulate a sensor moving over an image, with adjustable position and blurring.
 
@@ -22,10 +49,11 @@ class ImageEnv:
 
     def __init__(
         self,
-        image: torch.Tensor,
-        patch_size: int = 16,
+        img: torch.Tensor,
+        reward_function: RewardFunction,
         img_sensor_ratio: Optional[int] = None,
-        device: Optional[str] = "cpu"
+        device: Optional[str] = "cpu",
+        config: ActViTMAEConfig = None,
     ) -> None:
         """
         Initializes the ImageEnv with an image and sensor parameters.
@@ -38,31 +66,99 @@ class ImageEnv:
         if device is None:
             device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.device = device
-        self.img = image.to(self.device)
-        self.img_height, self.img_width = image.shape[2:]
+        self.img = img.to(self.device)
+        self.img_height, self.img_width = img.shape[2:]
 
         if img_sensor_ratio:
             self.sensor_height = self.img_height // img_sensor_ratio
             self.sensor_width = self.img_width // img_sensor_ratio
         else:
-            self.sensor_height = patch_size
-            self.sensor_width = patch_size
+            self.sensor_height = config.patch_size
+            self.sensor_width = config.patch_size
 
         self._sensor_min_pos = [0, 0]
         self.__sensor_pos = [rd.randint(0, self.img_height,), rd.randint(0, self.img_width,)]
 
         self._min_kernel_size = 1
-        self._max_kernel_size = self.img_height - self.sensor_height + 1
-        self._max_kernel_size = (
-            self._max_kernel_size if self._max_kernel_size % 2 == 1
-            else self._max_kernel_size - 1
-        )
+        self._max_kernel_size = make_kernel_size_odd(self.img_height - self.sensor_height + 1)
         self.__kernel_size = self._min_kernel_size
 
-        self._sampled_kernel_size_mask = torch.full_like(
-            self.img, fill_value=self._max_kernel_size, dtype=torch.int32
-        )
+        self._sampled_kernel_size_mask = torch.full_like(self.img, fill_value=self._max_kernel_size, dtype=torch.int32)
         self.sampled_img = torch.full_like(self.img, float('nan'), device=self.device)
+
+        # Gymnasium interface
+        self.observation_space = spaces.Box(
+            low=0,
+            high=255,
+            shape=(self.sensor_height, self.sensor_width, 3),
+            dtype="uint8",
+        )
+
+        self.action_space = spaces.Box(
+            low=np.array([-config.v_max_x, -config.v_max_y, -config.v_max_z], dtype=np.float32),
+            high=np.array([config.v_max_x, config.v_max_y, config.v_max_z], dtype=np.float32),
+            dtype=np.int32
+        )
+        self._max_steps = config.max_steps
+        self._step_count = 0
+
+        self.reward_function = reward_function
+        self.interval_reward_assignment = config.interval_reward_assignment
+        
+
+    def _get_obs(self):
+
+        return self.sampled_img
+
+    def _get_info(self):
+        info = {}
+
+        return info
+
+    def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
+        super().reset(seed=seed)
+
+        x = rd.randint(0, self.img_height)
+        y = rd.randint(0, self.img_width)
+
+        self._sensor_pos = (x, y)
+
+        z = make_kernel_size_odd(rd.randint(self._min_kernel_size, self._max_kernel_size))
+        self._kernel_size = z
+
+        # Clear episode variables
+        self.sampled_img = torch.full_like(self.img, float('nan'), device=self.device)
+        self._sampled_kernel_size_mask = torch.full_like(self.img, fill_value=self._max_kernel_size, dtype=torch.int32)
+        self._step_count = 0
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+        return observation, info
+
+    def step(self, action: Tuple[float, float, float]):
+
+        dx, dy, dz = action
+        self.move(dx, dy, dz)
+        observation = self._get_obs()
+
+        if self._step_count % self.interval_reward_assignment == 0:
+            reward = self.reward_function(self.img, self.sampled_img)
+
+        else:
+            reward = torch.tensor(0)
+
+        self._step_count += 1
+        terminated = (self._step_count == self._max_steps)
+        truncated = False
+        info = self._get_info()
+
+        return observation, reward, terminated, truncated, info
+    
+    @property
+    def step_count(self):
+
+        return self._step_count
 
     @property
     def fov_bbox(self) -> List[int]:
@@ -83,7 +179,7 @@ class ImageEnv:
         return [top, bottom, left, right]
 
     @property
-    def sensor_max_pos(self) -> Tuple[int, int]:
+    def _sensor_max_pos(self) -> Tuple[int, int]:
         """
         Computes the maximum valid position for the sensor based on the current FoV.
 
@@ -118,8 +214,8 @@ class ImageEnv:
             new_position (Tuple[int, int]): The new (y, x) position for the sensor.
         """
         x, y = new_position
-        self.__sensor_pos[0] = max(min(x, self.sensor_max_pos[0]), self._sensor_min_pos[0])
-        self.__sensor_pos[1] = max(min(y, self.sensor_max_pos[1]), self._sensor_min_pos[1])
+        self.__sensor_pos[0] = max(min(x, self._sensor_max_pos[0]), self._sensor_min_pos[0])
+        self.__sensor_pos[1] = max(min(y, self._sensor_max_pos[1]), self._sensor_min_pos[1])
 
     @property
     def _kernel_size(self) -> int:
@@ -132,19 +228,17 @@ class ImageEnv:
         return self.__kernel_size
 
     @_kernel_size.setter
-    def _kernel_size(self, kernel_size: int) -> None:
+    def _kernel_size(self, new_kernel_size: int) -> None:
         """
         Sets the kernel size, ensuring it stays within valid bounds.
 
         Args:
             kernel_size (int): The new kernel size.
         """
-        max_kernel_size_from_sensor_pos = min(self.img_height, self.img_width) - max(self._sensor_pos)
-        max_kernel_size_from_sensor_pos = (
-            max_kernel_size_from_sensor_pos if max_kernel_size_from_sensor_pos % 2 == 1
-            else max_kernel_size_from_sensor_pos - 1
-        )
-        self.__kernel_size = max(min(kernel_size, max_kernel_size_from_sensor_pos), self._min_kernel_size)
+        max_kernel_size_from_sensor_pos = make_kernel_size_odd(min(self.img_height, self.img_width) - max(self._sensor_pos))  # Current position restricts kernel size
+        new_kernel_size = max(min(new_kernel_size, max_kernel_size_from_sensor_pos), self._min_kernel_size)
+
+        self.__kernel_size = make_kernel_size_odd(new_kernel_size)
 
     def _apply_blur(self, window: torch.Tensor) -> torch.Tensor:
         """
@@ -161,7 +255,7 @@ class ImageEnv:
         window_padded = F.pad(window, (padding, padding, padding, padding), mode='reflect')
         blurred = F.avg_pool2d(window_padded, kernel_size=self._kernel_size, stride=1, padding=0)
 
-        assert blurred.shape == window.shape
+        assert blurred.shape == window.shape  # error here
 
         return blurred
 
@@ -191,7 +285,7 @@ class ImageEnv:
 
         return obs
 
-    def _observe(self) -> None:
+    def _update_sampled_img(self) -> None:
         """
         Updates the sampled image based on the current sensor position and kernel size.
         """
@@ -219,9 +313,9 @@ class ImageEnv:
             self._sensor_pos[0] + dy,
             self._sensor_pos[1] + dx,
         )
-        self._kernel_size += 2 * dz
+        self._kernel_size += dz
 
-        self._observe()
+        self._update_sampled_img()
 
     def random_walk(self, steps: int, planar_step_size: int = 10, altitude_step_size: int = 1):  # Temporal debug method
         for _ in range(steps):
@@ -232,11 +326,7 @@ class ImageEnv:
             self.move(dx, dy, dz)
 
 
-# MAE wrapper for reward computation
-class RewardFunction:
-    def __init__(self, model):
-        self.model = model  # Store the model instance
-
-    def __call__(self, state):
-        state_tensor = torch.tensor(state, dtype=torch.float32).unsqueeze(0)
-        return self.model(state_tensor).item()  # Compute reward
+# gym.register(
+#     id="ImgExploreEnv-v0",
+#     entry_point=f"{__name__}:ImageExplorationEnv",
+# )
